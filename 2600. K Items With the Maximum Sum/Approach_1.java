1b5f1051a11f356fdb1312ee6795e3de6d1992d6
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-08-28 11:10:35 +0530

    Easy: 2600. K Items With the Maximum Sum | Approach 1 | Runtime: 1 | Memory: 42892000

---
+++ 2600. K Items With the Maximum Sum/Approach_1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(k<=numOnes){
+            return k;
+        }else if(k<=numOnes+numZeros){
+            return numOnes;
+        }
+        int rem=k-(numZeros+numOnes);
+        return numOnes-rem;
+    }
+}
