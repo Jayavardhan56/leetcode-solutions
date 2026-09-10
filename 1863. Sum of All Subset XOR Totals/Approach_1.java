1de355bf5ae4bef581884cb107cba09efce62f80
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-09-10 14:05:16 +0530

    Easy: 1863. Sum of All Subset XOR Totals | Approach 1 | Runtime: 0 ms | Memory: 42708000

---
+++ 1863. Sum of All Subset XOR Totals/Approach_1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int res=0;
+        for(int i:nums){
+            res|=i;
+        }
+        return res*(1<<(nums.length-1));
+    }
+}
